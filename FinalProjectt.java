9194c31f04ab9bdc043af4924c9d961a4e30ac0e
ishhm24 <94818004+ishhm24@users.noreply.github.com>
2021-12-17 19:09:02 -0500

    Added my file

---
+++ FinalProjectt.java
@@ -0,0 +1,30 @@
+package FinalProjecttt;
+
+import java.util.Scanner;
+public class FinalProjectt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Please input your name: ");
+		String username = sc.nextLine();
+		username = username.toUpperCase();
+
+		System.out.println("What's your name?");
+		System.out.println("Address") ;
+		System.out.println("\nInput your phone number in (xxx-xxx-xxxx) format: ");
+		System.out.println("\nInput your email address: ");
+		System.out.println("\nTell us a little about yourself: ");
+		System.out.println("Thank you. We have your personal information.");
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+
+}
